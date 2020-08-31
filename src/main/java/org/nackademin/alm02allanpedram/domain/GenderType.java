6a28f1c18a03b3ca084051b2be320dc25db49ac1
Allan <allanjamil_91@hotmail.com>
2020-08-31 23:44:10 +0200

    added attributes to person class

---
+++ src/main/java/org/nackademin/alm02allanpedram/domain/GenderType.java
@@ -0,0 +1,7 @@
+package org.nackademin.alm02allanpedram.domain;
+
+public enum GenderType {
+    MALE,
+    FEMALE,
+    OTHER
+}
--- src/main/java/org/nackademin/alm02allanpedram/domain/Person.java
+++ src/main/java/org/nackademin/alm02allanpedram/domain/Person.java
@@ -1,4 +1,61 @@
 package org.nackademin.alm02allanpedram.domain;
 
 public class Person {
+
+    private String fullName;
+
+    private String nationality;
+
+    private int age;
+
+    private GenderType gender;
+
+    public Person(String fullName, String nationality, int age, GenderType gender) {
+        this.fullName = fullName;
+        this.nationality = nationality;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
